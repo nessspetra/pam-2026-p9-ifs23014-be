7184c850816030119c9109607ad8905e4ec470fd
Gayus Jones Petra <jonespetra102@gmail.com>
2026-04-26 20:09:29 +0700

    Commit keempat

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
 from app.config import Config
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Menjalankan aplikasi sesuai port di .env atau default 5000
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    # Menjalankan aplikasi sesuai port dari environment (untuk deployment) atau .env atau default 8080
+    port = int(os.environ.get("PORT", Config.APP_PORT))
+    app.run(host="0.0.0.0", port=port, debug=False)
